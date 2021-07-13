161d97af3d6814a9161e2f3f69ad4f6f0f8c2664
Johnny Ye <jye@squareup.com>
2021-07-13 12:38:16 -0400

    Adding more fake data

--- artists/fake_data.sql
+++ artists/fake_data.sql
@@ -25,7 +25,7 @@ INSERT INTO location (id,
                     latitude)
 VALUES (
     1,
-    "Brooklyn!",
+    "Brooklyn",
     0.1,
     0.1
 );
@@ -48,4 +48,108 @@ VALUES (
     "jayz@tidal.com",
     "129389812649",
     "jay.tidal.com"
+);
+
+INSERT INTO artist (id,
+                    name,
+                    location_id,
+                    location_range,
+                    price_id,
+                    contact_info_id,
+                    rating,
+                    genre,
+                    booking_count)
+VALUES (
+    2,
+    "Beyonce",
+    2,
+    1,
+    2,
+    2,
+    2,
+    2,
+    30
+);
+
+INSERT INTO location (id,
+                    name,
+                    longitude,
+                    latitude)
+VALUES (
+    2,
+    "Brooklyn",
+    0.1,
+    0.1
+);
+
+INSERT INTO price (id,
+                    price,
+                    pricing_type)
+VALUES (
+    2,
+    10000,
+    1
+);
+
+INSERT INTO contact_info (id,
+                    email,
+                    phone,
+                    website)
+VALUES (
+    2,
+    "beyonce@tidal.com",
+    "129389812649",
+    "beyonce.tidal.com"
+);
+
+INSERT INTO artist (id,
+                    name,
+                    location_id,
+                    location_range,
+                    price_id,
+                    contact_info_id,
+                    rating,
+                    genre,
+                    booking_count)
+VALUES (
+    3,
+    "Johnny",
+    3,
+    100,
+    3,
+    3,
+    5,
+    2,
+    1000
+);
+
+INSERT INTO location (id,
+                    name,
+                    longitude,
+                    latitude)
+VALUES (
+    3,
+    "Brooklyn",
+    0.1,
+    0.1
+);
+
+INSERT INTO price (id,
+                    price,
+                    pricing_type)
+VALUES (
+    3,
+    10000000,
+    1
+);
+
+INSERT INTO contact_info (id,
+                    email,
+                    phone,
+                    website)
+VALUES (
+    3,
+    "jye@squareup.com",
+    "222222222",
+    "jye.com"
 );
