604bf255ffc8b3255dba130c05307dc6445bc65a
Johnny Ye <jye@squareup.com>
2021-07-15 12:48:45 -0400

    rip grpc

--- artists/artists.py
+++ artists/artists.py
@@ -1,78 +1,62 @@
 # I have no idea what any of these imports are
-from concurrent import futures
-import random
 import pymysql
-import grpc
-from grpc_interceptor import ExceptionToStatusInterceptor
-from grpc_interceptor.exceptions import NotFound
-from artists_pb2 import (ArtistRequest,
-                         ArtistResponse,
-                         ArtistListResponse,
-                         AddArtistResponse)
-import artists_pb2_grpc
 from flask import Flask
 from flask_cors import CORS, cross_origin
-
+from flask import request
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+conn = pymysql.connect(
+    host='localhost',
+    user='user',
+    password="password",
+    db='tidalsurfartist',
+)
 
 @cross_origin
-@app.route("/get-artist/<user_id>")
+@app.route("/get-artist/<user_id>", methods=['GET'])
 def get_artists(user_id):
     # Initialize
-    return {'id': user_id}
-    response = ArtistResponse()
-    cur = self.conn.cursor()
+    cur = conn.cursor()
 
     # Update artist fields
-    cur.execute(f"select * from artist where id={request.id};")
+    cur.execute(f"select * from artist where id={user_id};")
     artist = cur.fetchone()
+    return {'response': artist}
 
-    response.artist.id = artist[0]
-    response.artist.name = artist[1]
-    response.artist.city = artist[2]
-    response.artist.price = artist[3]
-    response.artist.contact_info.email = artist[4]
-    response.artist.contact_info.phone = artist[5]
-    response.artist.contact_info.website = artist[6]
-    response.artist.rating = artist[7]
-    response.artist.genre = artist[8]
-    response.artist.booking_count = artist[9]
-    response.artist.type = artist[10]
-    return response
-
-
-@app.route("/add-artists")
-def add_artists():
+@app.route("/add-artist", methods=['POST'])
+def add_artist():
     # Initialize
-    response = AddArtistResponse()
     cur = conn.cursor()
 
     # Insert Data
-    cur.execute(f"INSERT INTO artist (name,\
-                                    city,\
-                                    price,\
-                                    email,\
-                                    phone,\
-                                    website,\
-                                    rating,\
-                                    genre,\
-                                    booking_count,\
-                                    type)\
-                    VALUES('{request.name}',\
-                            {request.city},\
-                            {request.price},\
-                            '{request.contact_info.email}',\
-                            '{request.contact_info.phone}',\
-                            '{request.contact_info.website}',\
-                            {request.rating},\
-                            {request.genre},\
-                            {request.booking_count},\
-                            {request.type});")
-    response.response = 1
-    return response
+    try:
+        cur.execute(f"INSERT INTO artist (name,\
+                                        city,\
+                                        price,\
+                                        email,\
+                                        phone,\
+                                        website,\
+                                        rating,\
+                                        genre,\
+                                        booking_count,\
+                                        type)\
+                        VALUES('{request.args.get('name')}',\
+                                '{request.args.get('city')}',\
+                                {request.args.get('price')},\
+                                '{request.args.get('email')}',\
+                                '{request.args.get('phone')}',\
+                                '{request.args.get('website')}',\
+                                {request.args.get('rating')},\
+                                {request.args.get('genre')},\
+                                {request.args.get('booking_count')},\
+                                {request.args.get('type')});")
+        conn.commit()
+    except pymysql.IntegrityError:
+        return {'response': 1}
+
+    return {'response': 0}
 
 
 @app.route("/get-nearby-artists")
